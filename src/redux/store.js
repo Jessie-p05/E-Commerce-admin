161a97333d19f880d34963fd46000f9d627442d8
Jessie-p05 <jxpei05@gmail.com>
2022-07-14 20:56:01 -0600

    setup user redux

---
+++ src/redux/store.js
@@ -0,0 +1,33 @@
+import {configureStore} from "@reduxjs/toolkit";
+import userReducer from "./userRedux";
+import storage from 'redux-persist/lib/storage';
+// import { combineReducers } from 'redux';
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+// const rootReducer = combineReducers({user:userReducer, cart:cartReducer});
+
+
+const persistedReducer = persistReducer(persistConfig, userReducer)
+
+export default configureStore({
+  reducer:persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+})
