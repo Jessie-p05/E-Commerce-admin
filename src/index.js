5db8607061bbe355ef7ec13ee3a2cc8a6d671ece
Jessie-p05 <jxpei05@gmail.com>
2022-07-17 20:16:01 -0600

    fromat this file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
@@ -10,9 +10,9 @@ let persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-   <PersistGate loading="null" persistor={persistor}>
-        <App />
-      </PersistGate>
+    <PersistGate loading="null" persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
