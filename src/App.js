0d082e1f5335bdde232f2956d5189f6b293876b9
Jessie-p05 <jxpei05@gmail.com>
2022-07-18 20:48:59 -0600

    change router v5 Swith hook to V6

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./App.css";
 import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
@@ -10,41 +16,36 @@ import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
 import Login from "./pages/login/login";
+import { useSelector } from "react-redux";
 
 function App() {
+  const loginUser = useSelector((state) => state.user.currentUser);
+  let admin;
+  if (loginUser) {
+    admin = useSelector((state) => state.user.currentUser.isAdmin);
+  }
+  let location = useLocation();
+  // const user = useSelector((state) => state.user.currentUser);
   return (
-    <Router>
-        <Switch>
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/users">
-            <UserList />
-          </Route>
-          <Route path="/user/:userId">
-            <User />
-          </Route>
-          <Route path="/newUser">
-            <NewUser />
-          </Route>
-          <Route path="/products">
-            <ProductList />
-          </Route>
-          <Route path="/product/:productId">
-            <Product />
-          </Route>
-          <Route path="/newproduct">
-            <NewProduct />
-          </Route>
+    <>
+      {location.pathname === "/login" ? null : <Topbar />}
+      <div className={location.pathname === "/login" ? null :"container"}>
+        {location.pathname === "/login" ? null : <Sidebar />}
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/users" element={<UserList />} />
+          <Route exact path="/user/:userId" element={<User />} />
+          <Route exact path="/newUser" element={<NewUser />} />
+          <Route exact path="/products" element={<ProductList />} />
+          <Route exact path="/product/:productId" element={<Product />} />
+          <Route exact path="/newproduct" element={<NewProduct />} />
+          <Route
+            path="/login"
+            element={admin ? <Navigate to="/" /> : <Login />}
+          />
+        </Routes>
       </div>
-        </Switch>
-    </Router>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+
 import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
@@ -10,9 +12,11 @@ let persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading="null" persistor={persistor}>
-      <App />
-    </PersistGate>
+    <BrowserRouter>
+      <PersistGate loading="null" persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
