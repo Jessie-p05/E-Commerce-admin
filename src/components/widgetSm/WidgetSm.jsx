290233150fc773e5da84f509ba216d19fff0f263
Jessie-p05 <jxpei05@gmail.com>
2022-07-19 15:42:02 -0600

    fetch users data and display on home page

--- src/components/widgetSm/WidgetSm.jsx
+++ src/components/widgetSm/WidgetSm.jsx
@@ -1,19 +1,32 @@
 import "./widgetSm.css";
 import { Visibility } from "@material-ui/icons";
+import { useEffect, useState } from "react";
+import { userRequest } from "../../requestMethods";
 
 export default function WidgetSm() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await userRequest.get("users/?new=true");
+        setUsers(res.data);
+      } catch {}
+    };
+    getUser()
+  }, []);
   return (
     <div className="widgetSm">
       <span className="widgetSmTitle">New Join Members</span>
       <ul className="widgetSmList">
-        <li className="widgetSmListItem">
+        {users.map((user)=>(
+        <li className="widgetSmListItem" key={user._id}>
           <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            src={user.img || "https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"}
             alt=""
             className="widgetSmImg"
           />
           <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
+            <span className="widgetSmUsername">{user.username}</span>
             <span className="widgetSmUserTitle">Software Engineer</span>
           </div>
           <button className="widgetSmButton">
@@ -21,6 +34,7 @@ export default function WidgetSm() {
             Display
           </button>
         </li>
+        ))}
         <li className="widgetSmListItem">
           <img
             src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
