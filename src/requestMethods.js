9bca5f5b652169047a20d4cd67ad7e5754e5380a
Jessie-p05 <jxpei05@gmail.com>
2022-07-19 21:27:54 -0600

    fix small bug

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
 let TOKEN;
-const loginUser = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser;
+const loginUser = JSON.parse(localStorage.getItem("persist:root"));
 console.log("hiiiii",loginUser)
 if(loginUser){
    TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
